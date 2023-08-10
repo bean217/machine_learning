Still need to fix backprop method since it is not working with batch size 1, also need to test the train method

FeedForward_ANNs/project/activation_functions.py
@@ -31,8 +31,8 @@ d_tanh = lambda x: 1 - tanh(x) ** 2
 # SOFTMAX
 # https://www.mldawn.com/the-derivative-of-softmaxz-function-w-r-t-z/
 
-softmax = lambda l, x: np.exp(x) / sum(np.exp(l))
+softmax = lambda x: np.exp(x) / sum(np.exp(x))
 
-def d_softmax(l, x):
-    act = softmax(l, x)
+def d_softmax(x):
+    act = softmax(x)
     return act * (1 - act)

FeedForward_ANNs/project/error_functions.py
@@ -8,11 +8,11 @@ mse = lambda y, y_pred: ((y - y_pred) ** 2)/2
 
 # MSE Last Layer Error Signal
 
-mse_l_esig = lambda d_act, l_in, y, y_pred: d_act(l_in) * (y - y_pred)
+mse_l_esig = lambda l_in, y, y_pred, d_act: d_act(l_in) * (y - y_pred)
 
 # MSE Hidden Layer Error Signal
 
-mse_h_esig = lambda esig, weights, d_act, l_in: \
+mse_h_esig = lambda l_in, esig, weights, d_act: \
     d_act(l_in) * np.asarray([sum(weights[:,i] * esig) for i in range(l_in.shape[0])])
 
 
@@ -31,5 +31,5 @@ mce_l_esig = lambda y, y_pred: (y_pred - y)
 
 # MSE Hidden Layer Error Signal
 
-mce_h_esig = lambda esig, weights, d_act, l_in: \
+mce_h_esig = lambda l_in, esig, weights, d_act: \
     d_act(l_in) * np.asarray([sum(weights[:,i] * esig) for i in range(l_in.shape[0])])

FeedForward_ANNs/project/neural_net.py
@@ -1,15 +1,7 @@
 import numpy as np
 
-from activation_functions import sigmoid, d_sigmoid
-from activation_functions import relu, d_relu
-from activation_functions import tanh, d_tanh
-from activation_functions import softmax, d_softmax
-
-from error_functions import mse, mse_l_esig, mse_h_esig
-from error_functions import bce
-from error_functions import mce, mce_l_esig, mce_h_esig
-
 from training_functions import Train_Funcs
+from training_functions import mean_square_error_sigmoid, mean_square_error_relu, mean_square_error_tanh, multiple_cross_entropy_softmax
 
 def train_test_split(data: np.ndarray, labels: np.ndarray, validation_percentage: float = 0.33):
     """Splits training data into a training and validation set
@@ -75,35 +67,51 @@ class ANN:
         """
         # set delta_weights - list of layer weight matrices update values
         del_w = [np.zeros(shape=layer.weights.shape) for layer in self.layers]
+        # TODO:
+        # total error
+        err_tot = 0
 
         # calculate the error signal of the last layer
         for i in range(batch_X.shape[0]):
             # perform forward pass on training data instance
             ins, out = self.forward(batch_X[i])
             
-            
+            # TODO:
             # # get network error for the training data instance
-            # err = self.tfs.err(batch_y[i], out[-1])
+            err_tot += sum(self.tfs.err(batch_y[i], out[-1])) # sum up over all output neurons
             
             # calculate input for each neuron in the last layer
 
             # calculate the error signal of the last layer
             # pass in predicted output, layer output, and layer inputs
-            e_sig = self.tfs.L_esig(batch_y[i], out[-1], ins[-1])
+            e_sig = self.tfs.L_esig(ins[-1], batch_y[i], out[-1])
             
-            for j in range(len(self.layers), -1, -1):
+            for j in range(len(self.layers)-1, -1, -1):
                 # calculate change in weights for this layer
-                del_w[j] += np.asarray([-learning_rate * e_sig[k] * out[j] for k in range(e_sig.shape[0])])
+                dw = np.asarray([learning_rate * e_sig[k] for k in range(e_sig.shape[0])])
+                dw = np.asarray([dw] + [dw * out[j][i] for i in range(len(out[j]))])
+                #print(dw.T)
+                del_w[j] += dw.T
+                #TODO: REMOVE: del_w[j] += np.asarray([-learning_rate * e_sig[k] * out[j][] for k in range(e_sig.shape[0])])
                 # get the previous layer's error signal
                 # pass in successor layer's error signal, the layer's activations, 
                 #   and the weight matrix between the layer and it's successor layer
-                e_sig = self.tfs.H_esig(e_sig, self.layers[j].weights, ins[j-1])
+                e_sig = self.tfs.H_esig(ins[j-1], e_sig, self.layers[j].weights)
         
         # after all weight updates have been found, apply them
+        # for i in range(len(self.layers)):
+        #     print("Layer",i)
+        #     print(self.layers[i].weights)
+        # print()
         for i in range(len(self.layers)):
             self.layers[i].weights += del_w[i]
+        # print()
+        # for i in range(len(self.layers)):
+        #     print("Layer",i)
+        #     print(self.layers[i].weights)
 
-
+        # TODO:
+        return err_tot
 
 
     def train(self, epochs: int, batch_size: int, learning_rate: float, 
@@ -143,7 +151,7 @@ class ANN:
 
         for e in range(epochs):
             # generate indices of random training data sample/mini-batch
-            batch_indices = np.random.choice(self.train_X.shape[0], batch_size, replace=False)
+            batch_indices = np.random.choice(self.train_X.shape[0], bs, replace=False)
             error = self.backprop(train_X[batch_indices], train_y[batch_indices], learning_rate)
 
             # use this section for plotting error during training
@@ -162,7 +170,7 @@ class Layer:
             @param architecture: 2-tuple representing (dim_in, dim_out) dimensions
         """
         # weights represents the weight matrix w_(k, k-1)
-        self.weights = np.random.normal(size=(architecture[1], architecture[0]+1))
+        self.weights = np.ones(shape=(architecture[1], architecture[0]+1)) * 1.5#TODO: CHANGE BACK: np.random.normal(size=(architecture[1], architecture[0]+1))
         self.dim_in = architecture[0]
         self.dim_out = architecture[1]
         print(self.weights)
@@ -171,18 +179,41 @@ class Layer:
 
 def main():
     # set training function parameters
-    sigmoid = lambda x: 1/(1+np.exp(-x))
-    d_sigmoid = lambda x: sigmoid(x) * (1 - sigmoid(x))
-    err = lambda y, y_pred: ((y - y_pred) ** 2)/2
-    tfs = Train_Funcs(sigmoid, d_sigmoid, err, None, None)
-    # create network
-    ann = ANN(tfs, (2, 3, 2))
-    #print(ann.forward(np.asarray([1, 1])))
-    
-    l = np.asarray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    # sigmoid = lambda x: 1/(1+np.exp(-x))
+    # d_sigmoid = lambda x: sigmoid(x) * (1 - sigmoid(x))
+    # err = lambda y, y_pred: ((y - y_pred) ** 2)/2
+    # tfs = Train_Funcs(sigmoid, d_sigmoid, err, None, None)
 
-    idxs = np.random.choice(l.shape[0], 2, replace=False) 
-    print(l[idxs])
+    # create network
+    ann = ANN(mean_square_error_sigmoid, (2, 2, 1))
+    
+    res = ann.forward(np.asarray([1, 1]))
+
+    print("ins", res[0])
+    print("outs", res[1])
+    
+
+    for i in range(2500):
+        x = np.array([[1, 1], [1, 0], [0, 1], [0, 0]])
+        l = np.array([1, 0, 0, 0])
+        for j in range(4):
+
+            err = ann.backprop(np.array(x[j]),
+                         np.array(l[j]),
+                         0.1)
+        if i == 0 or i == 2499:
+            print("ERROR:", err)
+
+
+    res = ann.forward(np.asarray([1, 1]))
+
+    print("ins", res[0])
+    print("outs", res[1])
+
+    # l = np.asarray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+    # idxs = np.random.choice(l.shape[0], 2, replace=False) 
+    # print(l[idxs])
 
     pass
 